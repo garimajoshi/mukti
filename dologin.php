768d6e0bf198eb2743ba6e640f2f55cbf92ee344
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-22 23:44:54 -0800

    Update dologin.php

--- dologin.php
+++ dologin.php
@@ -3,15 +3,34 @@
     session_start();
     if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        if($_POST['_register'])
+        if(isiset($_POST['_register']))
         {
             //input validation and register user
-
+            
         }
-        else if($_POST['_login'])
+        else if(isset($_POST['_login']))
         {
             //check login credentials
+            $myemailid = addslashes($_POST['email']);
+            $mypassword = addslashes($_POST['password']);
 
+            $sql = "SELECT email_id FROM registered_users WHERE email_id='$myemailid' and password='$mypassword'";
+            $result = mysql_query($sql);
+            $row = mysql_fetch_array($result);
+            $myname = $row['name'];
+            $count = mysql_num_rows($result);
+            
+            if($count == 1)
+            {
+                session_register("myusername");
+                $_SESSION['login_email_id'] = $myemailid;
+                $_SESSION['login_name'] = $myname;
+                header("location: index.html");
+            }
+            else
+            {
+                $error="Invalid username/password";
+            }
         }
         else 
         {
