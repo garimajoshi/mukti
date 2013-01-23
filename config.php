264bd5ab7980f259572430159197c57a7cf92004
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-23 04:18:25 -0800

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,6 @@
     $mysql_password = "";
     $mysql_database = "mukti13";
     $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password)
-    or die("Oops some thing went wrong");
-    mysql_select_db($mysql_database, $bd) or die("Oops some thing went wrong");
+    or die(mysql_error());
+    mysql_select_db($mysql_database, $bd) or die(mysql_error());
 ?>
