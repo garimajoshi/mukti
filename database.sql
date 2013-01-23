5331895822183c4df48bb9217a2ccb8534fea4db
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-24 05:11:02 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE mukti CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `registered_users` (
+    `id` int NOT NULL auto_increment,
     `email_id` varchar(30) NOT NULL,
     `name` varchar(30) NOT NULL,
     `college` varchar(50) NOT NULL,
