971391de308c3ff5277c501ace9fcdda8b3e103d
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-24 05:16:01 +0530

    Update dologin.php

--- dologin.php
+++ dologin.php
@@ -22,7 +22,7 @@
                 // registered successfully
                 $_SESSION['login_email_id'] = $myemailid;
                 $_SESSION['login_name'] = $myname;
-				header("location: index.php");
+		header("location: index.php");
             }
             else
             {
@@ -36,16 +36,18 @@
             $myemailid = addslashes($_POST['email']);
             $mypassword = addslashes($_POST['password']);
             $mypassword = md5($mypassword);
-            $sql = "SELECT email_id, name FROM registered_users WHERE email_id='$myemailid' and password='$mypassword'";
+            $sql = "SELECT email_id, name, id FROM registered_users WHERE email_id='$myemailid' and password='$mypassword'";
             $result = mysql_query($sql) or die(mysql_error());
             $row = mysql_fetch_array($result);
             $myname = $row['name'];
+            $userid = $row['id'];
             $count = mysql_num_rows($result);
             
             if($count == 1)
             {
                 $_SESSION['login_email_id'] = $myemailid;
                 $_SESSION['login_name'] = $myname;
+                $_SESSION['user_id'] = $userid;
                 header("location: index.php");
             }
             else
