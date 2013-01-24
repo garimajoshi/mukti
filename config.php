3f3040b336c43cff1fdd21e889bfc00a9ffa4d4b
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-24 17:11:48 +0530

    removed uneccessary files

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $mysql_hostname = "localhost";
-    $mysql_user = "mukti13_user";
-    $mysql_password = "mukti13_pass";
-    $mysql_database = "mukti13";
-    $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password)
-    or die(mysql_error());
-    mysql_select_db($mysql_database, $bd) or die(mysql_error());
-?>
