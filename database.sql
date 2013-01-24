1e08c7e2b5921d8fe1596dd5e5da7764b3ef3457
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-24 16:31:44 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS `registered_users` (
     `id` int NOT NULL auto_increment,
     `email_id` varchar(30) NOT NULL,
     `name` varchar(30) NOT NULL,
+    `confirmation_code` varchar(50) NOT NULL,
     `college` varchar(50) NOT NULL,
     `phone` varchar(20) NOT NULL,
     `password` varchar(50) NOT NULL,
