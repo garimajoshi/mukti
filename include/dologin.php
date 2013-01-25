cf982d87e428ef293cc4c23e46a192ff2f4ec8e6
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-25 21:50:36 +0530

    Update include/dologin.php

--- include/dologin.php
+++ include/dologin.php
@@ -20,27 +20,30 @@
 				echo $error;
 			}
 			
-			if($count == 0)
-			{
-				$myname = addslashes($_POST['name']);
-				$mypassword = addslashes($_POST['password']);
-				$mypassword = md5($mypassword);
-				$myphone = addslashes($_POST['phone']);
-				$mydepartment = addslashes($_POST['department']);
-				$mycity = addslashes($_POST['city']);
-				$myyear = addslashes($_POST['year_of_study']);
-				$mycollege = addslashes($_POST['college']);
-				$confirmationcode = MakeConfirmationMd5($myemailid);
-				mysql_query("INSERT INTO registered_users(email_id, name, college, phone, password, confirmation_code, department, year_of_study, city) VALUES('$myemailid', '$myname', '$mycollege', '$myphone', '$mypassword', '$confirmationcode', '$mydepartment', '$myyear', '$mycity')") or die(mysql_error());
-
-				// registered successfully
-				$message = "Successfully Registered, check your mail for activation";
-				echo $message;
-			}
 			else
 			{
-				$error = "Email already registered";
-				echo $error;
+				if($count == 0)
+				{
+					$myname = addslashes($_POST['name']);
+					$mypassword = addslashes($_POST['password']);
+					$mypassword = md5($mypassword);
+					$myphone = addslashes($_POST['phone']);
+					$mydepartment = addslashes($_POST['department']);
+					$mycity = addslashes($_POST['city']);
+					$myyear = addslashes($_POST['year_of_study']);
+					$mycollege = addslashes($_POST['college']);
+					$confirmationcode = MakeConfirmationMd5($myemailid);
+					mysql_query("INSERT INTO registered_users(email_id, name, college, phone, password, confirmation_code, department, year_of_study, city) VALUES('$myemailid', '$myname', '$mycollege', '$myphone', '$mypassword', '$confirmationcode', '$mydepartment', '$myyear', '$mycity')") or die(mysql_error());
+
+					// registered successfully
+					$message = "Successfully Registered, check your mail for activation";
+					echo $message;
+				}
+				else
+				{
+					$error = "Email already registered";
+					echo $error;
+				}
 			}
 		}
 		else if(isset($_POST['signin']))
