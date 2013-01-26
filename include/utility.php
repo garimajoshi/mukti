23389084077ad53ef5aa4d2904a8a0862bb2bcbd
Garima Joshi <garimajoshi3@yahoo.in>
2013-01-26 14:39:46 +0530

    Update include/utility.php

--- include/utility.php
+++ include/utility.php
@@ -93,7 +93,7 @@ function ValidateRegistrationSubmission($myemail, $myname, $mypassword, $myphone
         $toReturn = false;
         exit();
     }
-    if(isset($myphone) && (false == preg_match("/^([1]-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$/i", $myphone)) || strlen($myphone)>20)
+    if(isset($myphone) && strlen($myphone)>15)
     {
         echo 'Please enter valid phone number';
         $toReturn = false;
